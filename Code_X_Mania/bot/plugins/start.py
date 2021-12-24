6900731b7b9157e822acd6b6da29c4db33150f00
me0wx <94188482+me0wx@users.noreply.github.com>
2021-12-24 15:55:05 +0530

    Update start.py

--- Code_X_Mania/bot/plugins/start.py
+++ Code_X_Mania/bot/plugins/start.py
@@ -43,7 +43,7 @@ async def start(b, m):
             except UserNotParticipant:
                 await b.send_message(
                     chat_id=m.chat.id,
-                    text="<i>Also Join our channel To Get movies\nYou can use this bot now!\n😁 🔐</i>",
+                    text="<i>⚽Also Join our channel To Get movies❤\n✅You can use this bot now!\n😁 �</i>",
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
